c3351e47917138bc034526d67645a5a7b69c5f95
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-02-26 07:25:36 +0530

    Create diffsum.cpp

---
+++ diffsum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int a;
+	int n1;
+	int n2;
+	cin>>n1>>n2;
+	if(n1>n2)
+	a=n1-n2;
+	else
+	a=n1+n2;
+	
+	cout<<a;
+	return 0;
+}
